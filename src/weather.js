9c48bb179a8a1aab7eae0185607be56b6472f867
margaridamedinacosta <57046731+margaridamedinacosta@users.noreply.github.com>
2019-11-24 17:14:55 +0000

    add axios

---
+++ src/weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h2>hello from Weather</h2>;
+    </div>
+  );
+}
